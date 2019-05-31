8bc771a32526f75393ca1fa91f169c475a3a030f
alizajaz <38137562+alizajaz@users.noreply.github.com>
2019-05-31 10:57:52 -0500

    Update App.js

--- Book/src/App.js
+++ Book/src/App.js
@@ -9,16 +9,72 @@ import Error404 from './Components/Pages/Error404';
 class BooksApp extends React.Component {
   render() {
     return(
-      <BrowserRouter>
-      <Switch>
-       <Route exact path="/" component={ HomePage } />
-       <Route exact path="/search" component={ searchPage } />
-        <Route exact path ="/*" component ={ Error404 } />
-
-        </Switch>
-        </BrowserRouter>
-    );
+      
+      
+      
+      state = {
+    books: []
   }
+
+  componentDidMount() {
+   this.addbooks()
+ }
+
+
+
+ addBooks = () => {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books });
+    })
+  };
 }
+updateShelf = (book, shelf) => {
+ BooksAPI.update(book, shelf).then(response => {
+   this.addbooks()
+ })
+}
+
+  render() {
+
+    return(
+    <div className="app">
+       <Route exact path="/"  render={() =>(
+         <HomePage
+          books={this.state.books}
+           updateShelf={this.updateShelf}
+           />
+         )}
+         />
+
+       <Route
+       exact
+       path="/search"
+       render={() => (
+        <searchPage
+         updateShelf={this.updateShelf}
+          books={this.state.books}
+        />
+      )}
+      />
+
+        <Route exact path ="/*" component ={ Error404 } />
+          </div>
+      )
+    }
+
+}
+
+// export default BooksApp
+//       <BrowserRouter>
+//       <Switch>
+//        <Route exact path="/" component={ HomePage } />
+//        <Route exact path="/search" component={ searchPage } />
+//         <Route exact path ="/*" component ={ Error404 } />
+
+//         </Switch>
+//         </BrowserRouter>
+//     );
+//   }
+// }
 
 export default BooksApp
